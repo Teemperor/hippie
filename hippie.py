e0b22799981c80b9bd688708d6b3dc34c2bb3bcc
Raphael Isemann <teemperor@gmail.com>
2017-03-28 16:17:18 +0200

    Added OS X compability

--- hippie.py
+++ hippie.py
@@ -7,15 +7,23 @@ import re
 import shutil
 import errno
 
+is_osx = (sys.platform == "darwin")
+
+compiler = "clang"
+
+preload_flag = "LD_PRELOAD="
+if is_osx:
+  preload_flag = "DYLD_FORCE_FLAT_NAMESPACE=y DYLD_INSERT_LIBRARIES="
+
 cwd = os.path.realpath(os.getcwd());
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 store_source_path = script_dir + "/storeenv.c"
 chroot_source_path = script_dir + "/chroot.c"
 
+cmdline = (" ".join(sys.argv[1:]))
+
 log_path = cwd + "/log"
-cmdline_path = cwd + "/cmdline"
-cwdfile_path = cwd + "/cwd"
 chroot_so_path = cwd + "/chroot.so"
 store_so_path = cwd + "/storeenv.so"
 output_dir = cwd + "/standalone/"
@@ -37,66 +45,29 @@ def print_and_run(command):
     return os.system(command)
 
 def compile_shared_libs():
-    if print_and_run("gcc -fPIC -shared " + store_source_path + " -o " + store_so_path + " -ldl") != 0:
+    if print_and_run(compiler + " -fPIC -shared " + store_source_path + " -o " + store_so_path + " -ldl") != 0:
         print("error while compiling. Aborting...")
         exit(1)
-    if print_and_run("gcc -fPIC -shared " + chroot_source_path + " -o " + chroot_so_path + " -ldl") != 0:
+    if print_and_run(compiler + " -fPIC -shared " + chroot_source_path + " -o " + chroot_so_path + " -ldl") != 0:
         print("error while compiling. Aborting...")
         exit(1)
 
 def create_reproduce_script():
-    f = open(cmdline_path)
-    cmdline_line = f.readlines()[0].strip()
-    f.close()
-    executable = cmdline_line.split(" ")[0]
-    shutil.copyfile(executable, output_dir + executable)
-    shutil.copystat(executable, output_dir + executable)
-    
     shutil.copyfile(chroot_so_path, output_dir + "/chroot.so")
-    
-    f = open(cwdfile_path)
-    chroot_cwd = f.readlines()[0].strip()
-    f.close()
     
     with open(run_script, "w") as f:
         f.write("#!/bin/bash\n")
         f.write("set +e\n")
         f.write("""DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"\n""")
-        f.write('CHRSO=$(realpath chroot.so)\n')
+        f.write('CHRSO="$DIR/chroot.so"\n')
         f.write('cd "$DIR"\n')
-        f.write('cd "' + chroot_cwd[1:] + '"\n')
-        f.write("GP_CHROOT_PATH=\"$DIR/\" LD_PRELOAD=\"$CHRSO\" " + cmdline_line + "\n")
+        f.write('cd "' + cwd[1:] + '"\n')
+        f.write("GP_CHROOT_PATH=\"$DIR/\" " + preload_flag + "\"$CHRSO\" " + cmdline + "\n")
         f.write('exit 0\n')
-
-def create_creduce_runner_template():
-    f = open(cmdline_path)
-    cmdline_line = f.readlines()[0].strip()
-    f.close()
-    f = open(cwdfile_path)
-    chroot_cwd = f.readlines()[0].strip()
-    f.close()
-    
-    with open(output_dir + creduce_runner_template, "w") as f:
-        f.write('if [ -f main.cpp ]; then\n  exit 1\nfi\n')
-        f.write('rsync  --exclude "*.orig" --exclude "$BASEDIR/creduce_bug_*" --ignore-existing -aAXrq "$BASEDIR"/* . >/dev/null 2>/dev/null\n')
-        f.write("bash run.sh 2>err 1>out\n")
-        f.write("grep -q goodstring err\n")
-        
-def create_creduce_starter():
-    with open(creduce_starter, "w") as f:
-        f.write("#!/bin/bash\n")
-        f.write("""DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"\n""")
-        f.write('cd "$DIR"\n')
-        f.write('echo "#!/bin/bash" > ' + creduce_runner + "\n")
-        f.write('echo "BASEDIR=$DIR" >> ' + creduce_runner + "\n")
-        f.write('cat ' + creduce_runner_template + ' >> ' + creduce_runner + "\n")
-        f.write('chmod +x ' + creduce_runner + '\n')
-        #f.write("find . -type f | xargs -L1 file -F ' ' | grep ' text' | grep -vF 'test.sh' | grep -vF 'run.sh' | grep -vF 'creduce.sh' | awk '{print $1}' | xargs creduce --debug ./test.sh\n")
-        f.write("creduce --debug ./test.sh ./\n")
     
 
 def run_user_command():
-    print_and_run("GP_CWD=" + cwdfile_path + " GP_CMDLINE=" + cmdline_path + " GP_STORAGE=" + log_path + " LD_PRELOAD=" + store_so_path + " " + (" ".join(sys.argv[1:])))
+    print_and_run("GP_CWD=" + cwd + " GP_STORAGE=" + log_path + " " + preload_flag + store_so_path + " " + (" ".join(sys.argv[1:])))
 
 class SystemCall:
     callType = ""
@@ -148,11 +119,6 @@ try:
 except:
     pass
 
-try:
-    os.remove(cmdline_path)
-except:
-    pass
-
 compile_shared_libs()
 
 run_user_command()
@@ -160,7 +126,5 @@ run_user_command()
 handle_log()
 
 create_reproduce_script()
-create_creduce_runner_template()
-create_creduce_starter()
 
 print("DONE!")
--- storeenv.c
+++ storeenv.c
@@ -12,8 +12,6 @@
 
 #define MAX_PATH_LEN 4096
 #define STORAGE "GP_STORAGE"
-#define CMDLINE "GP_CMDLINE"
-#define CWDFILE "GP_CWD"
 
 struct stat;
 
@@ -26,10 +24,9 @@ static void store_str(const char* content) {
     assert(getenv(STORAGE));
     FILE *f = fopen(getenv(STORAGE), "a");
     assert(f != 0);
-    fprintf(f, content);
+    fprintf(f, "%s", content);
     fclose(f);
 }
-
 
 static int startsWith(const char *pre, const char *str) {
     size_t lenpre = strlen(pre),
@@ -49,189 +46,15 @@ static void store_pathname(const char* pathname) {
     store_str("\n");
 }
 
-static int isClangCC1() {
-    FILE *file = fopen("/proc/self/cmdline", "r");
-    int c;
-    int index = 0;
-    int lastWas0 = 1;
-
-    if (file == 0) {
-        fprintf(stderr, "isClangCC1: (/proc/self/cmdline, 'r') failed: %s\n", strerror(errno));
-        exit(1);
-    }
-
-    while ((c = fgetc(file)) != EOF)
-    {
-        if (c == '\0') {
-            if (lastWas0) {
-                fclose(file);
-                return 0;
-            }
-            lastWas0 = 1;
-        } else {
-            lastWas0 = 0;
-        }
-        switch(index) {
-        case 0:
-            if (c == '-') {
-                index++;
-            }
-            break;
-        case 1:
-            if (c == 'c') {
-                index++;
-            } else {
-                index = 0;
-            }
-            break;
-        case 2:
-            if (c == 'c') {
-                index++;
-            } else {
-                index = 0;
-            }
-            break;
-        case 3:
-            if (c == '1') {
-                fclose(file);
-                return 1;
-            } else {
-                index = 0;
-            }
-            break;
-        default:
-            break;
-        }
-    }
-    fclose(file);
-    return 0;
-}
-
-static void saveCwdFile() {
-    assert(getenv(CWDFILE));
-    if (access (getenv(CWDFILE), F_OK ) != -1) {
-    } else {
-        FILE *outfile = fopen(getenv(CWDFILE), "a");
-        assert(outfile != 0);
-        
-        if (outfile == 0) {
-            fprintf(stderr, "fopen('getenv(CWDFILE)', 'a') failed: %s\n", strerror(errno));
-            exit(1);
-        }
-        
-        char cwd[MAX_PATH_LEN];
-        if (getcwd(cwd, MAX_PATH_LEN))
-            assert("getcwd failed");
-        
-        size_t len = strlen(cwd);
-        
-        for (size_t i = 0; i < len; ++i)
-            fprintf(outfile, "%c", cwd[i]);
-
-        fclose(outfile);
-    }
-}
-
-static void saveCmdLine() {
-    assert(getenv(CMDLINE));
-    if (access (getenv(CMDLINE), F_OK ) != -1) {
-    } else {
-        FILE *outfile = fopen(getenv(CMDLINE), "a");
-        assert(outfile != 0);
-        
-        if (outfile == 0) {
-            fprintf(stderr, "fopen('getenv(CMDLINE)', 'a') failed: %s\n", strerror(errno));
-            exit(1);
-        }
-        
-        FILE *infile = fopen("/proc/self/cmdline", "r");
-        int c;
-        
-        if (infile == 0) {
-            fprintf(stderr, "saveCmdLine: fopen(/proc/self/cmdline, 'r') failed: %s\n", strerror(errno));
-            exit(1);
-        }
-
-        while ((c = fgetc(infile)) != EOF)
-        {
-            if (c == 0)
-                fprintf(outfile, " ");
-            else
-                fprintf(outfile, "%c", c);
-        }
-        fprintf(outfile, "\n");
-        
-        fclose(infile);
-        fclose(outfile);
-    }
-}
-
-static int isClang() {
-    char exe[1024];
-    int ret;
-    
-    ret = readlink("/proc/self/exe", exe, sizeof(exe) - 1);
-    if (ret ==-1) {
-        fprintf(stderr, "readlink(/proc/self/exe, ...) failed\n");
-        exit(1);
-    }
-    exe[ret] = 0;
-    if (  // check for clang-X.X pattern
-          (ret >= 9 &&
-           exe[ret-1] >= '0' && exe[ret-1] <= '9' &&
-           exe[ret-2] == '.' &&
-           exe[ret-3] >= '0' && exe[ret-3] <= '9' &&
-           exe[ret-4] == '-' &&
-           exe[ret-5] == 'g' &&
-           exe[ret-6] == 'n' &&
-           exe[ret-7] == 'a' &&
-           exe[ret-8] == 'l' &&
-           exe[ret-9] == 'c') ||
-          // check for /clang (in case people don't do symlinks).
-          (ret >= 5 &&
-           exe[ret-1] == 'g' &&
-           exe[ret-2] == 'n' &&
-           exe[ret-3] == 'a' &&
-           exe[ret-4] == 'l' &&
-           exe[ret-5] == 'c') ||
-          // check for /clang++ (in case people don't do symlinks).
-          (ret >= 7 &&
-           exe[ret-1] == '+' &&
-           exe[ret-2] == '+' &&
-           exe[ret-3] == 'g' &&
-           exe[ret-4] == 'n' &&
-           exe[ret-5] == 'a' &&
-           exe[ret-6] == 'l' &&
-           exe[ret-7] == 'c')
-       ) {
-        if (isClangCC1()) {
-            saveCmdLine();
-        }
-        saveCwdFile();
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
 int open(const char *pathname, int flags, mode_t mode) {
     orig_open_f_type orig;
     orig = (orig_open_f_type)dlsym(RTLD_NEXT, "open");
     
-    if (strcmp(pathname, getenv(CMDLINE)) == 0) {
-        return orig(pathname, flags, mode);
-    }
     if (strcmp(pathname, getenv(STORAGE)) == 0) {
-        return orig(pathname, flags, mode);
-    }
-    if (strcmp(pathname, getenv(CWDFILE)) == 0) {
         return orig(pathname, flags, mode);
     }
     
     if (startsWith("/proc/", pathname) || startsWith("/dev/", pathname)) {
-        return orig(pathname, flags, mode);
-    }
-    if (!isClang()) {
         return orig(pathname, flags, mode);
     }
     
@@ -245,25 +68,19 @@ int open(const char *pathname, int flags, mode_t mode) {
     }
 }
 
-
-
+#ifdef __linux__ 
 int __xstat(int ver, const char *pathname, struct stat *buf) {
+#else
+int xstat(int ver, const char *pathname, struct stat *buf) {
+#endif
     orig_stat_f_type orig;
     orig = (orig_stat_f_type)dlsym(RTLD_NEXT, "__xstat");
-    if (strcmp(pathname, getenv(CMDLINE)) == 0) {
-        return orig(ver, pathname, buf);
-    }
-    if (strcmp(pathname, getenv(CWDFILE)) == 0) {
-        return orig(ver, pathname, buf);
-    }
+
     if (startsWith("/proc/", pathname) || startsWith("/dev/", pathname)) {
         return orig(ver, pathname, buf);
     }
     
     if (getenv(STORAGE)) {
-        if (!isClang()) {
-            return orig(ver, pathname, buf);
-        }
         store_str("stat ");
         store_pathname(pathname);
         return orig(ver, pathname, buf);
@@ -274,23 +91,19 @@ int __xstat(int ver, const char *pathname, struct stat *buf) {
 }
 
 
+#ifdef __linux__ 
 int __xlstat(int ver, const char *pathname, struct stat *buf) {
+#else
+int xlstat(int ver, const char *pathname, struct stat *buf) {
+#endif
     orig_lstat_f_type orig;
     orig = (orig_lstat_f_type)dlsym(RTLD_NEXT, "__xlstat");
-    if (strcmp(pathname, getenv(CMDLINE)) == 0) {
-        return orig(ver, pathname, buf);
-    }
-    if (strcmp(pathname, getenv(CWDFILE)) == 0) {
-        return orig(ver, pathname, buf);
-    }
+
     if (startsWith("/proc/", pathname) || startsWith("/dev/", pathname)) {
         return orig(ver, pathname, buf);
     }
     
     if (getenv(STORAGE)) {
-        if (!isClang()) {
-            return orig(ver, pathname, buf);
-        }
         store_str("lstat ");
         store_pathname(pathname);
         return orig(ver, pathname, buf);
@@ -304,20 +117,12 @@ int __xlstat(int ver, const char *pathname, struct stat *buf) {
 int access(const char *pathname, int mode) {
     orig_access_f_type orig;
     orig = (orig_access_f_type)dlsym(RTLD_NEXT, "access");
-    if (strcmp(pathname, getenv(CMDLINE)) == 0) {
-        return orig(pathname, mode);
-    }
-    if (strcmp(pathname, getenv(CWDFILE)) == 0) {
-        return orig(pathname, mode);
-    }
+
     if (startsWith("/proc/", pathname) || startsWith("/dev/", pathname)) {
         return orig(pathname, mode);
     }
     
     if (getenv(STORAGE)) {
-        if (!isClang()) {
-            return orig(pathname, mode);
-        }
         store_str("access ");
         store_pathname(pathname);
         return orig(pathname, mode);
